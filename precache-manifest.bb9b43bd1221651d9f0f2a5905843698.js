be660bf3b9fdeb8cede22dc06ca57d3b9d39ad2e
Pawel Borowik <pawelbor1@op.pl>
2020-02-13 00:19:38 +0100

    Updates

---
+++ precache-manifest.bb9b43bd1221651d9f0f2a5905843698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cdf92b12fe98bfbce40c8e3883b10e",
+    "url": "/AppMakePizza/index.html"
+  },
+  {
+    "revision": "1708230f4d637d09e8ac",
+    "url": "/AppMakePizza/static/css/main.b526e955.chunk.css"
+  },
+  {
+    "revision": "891ed48c9775ea3335e9",
+    "url": "/AppMakePizza/static/js/2.4c02bc2c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/AppMakePizza/static/js/2.4c02bc2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1708230f4d637d09e8ac",
+    "url": "/AppMakePizza/static/js/main.8b8a410e.chunk.js"
+  },
+  {
+    "revision": "ca36b6a124643ad145bd",
+    "url": "/AppMakePizza/static/js/runtime-main.15c16ebd.js"
+  }
+]);
